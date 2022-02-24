822ffe3e08846f0605af56a1966f75d791f6961b
Mengyi Wang <mengyi.wang@canonical.com>
2022-02-24 15:30:52 +0100

    Add indentation inside if statement
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/runtime-helpers/bin/redis-token-setup.sh
+++ snap/local/runtime-helpers/bin/redis-token-setup.sh
@@ -9,19 +9,19 @@ CONNECTIONS_FILE=$SNAP_DATA/etc/connections/connection.yaml
 
 # use Vault token query Redis token, access edgexfoundry secure Message Bus
 if [ -f "$VAULT_TOKEN_FILE" ] ; then
-# get Vault token and create redis.yaml
-logger "edgex-ekuiper:redis-token-setup: using Vault token to query Redis token"
-TOKEN=$(yq "$VAULT_TOKEN_FILE" | yq ' .auth.client_token')
-touch "$REDIS_TOKEN_FILE"
+	# get Vault token and create redis.yaml
+	logger "edgex-ekuiper:redis-token-setup: using Vault token to query Redis token"
+	TOKEN=$(yq "$VAULT_TOKEN_FILE" | yq ' .auth.client_token')
+	touch "$REDIS_TOKEN_FILE"
 
-# check if curl work
-CURL_HTTP_CODE=$(curl -o /dev/null --write-out "%{http_code}" -s --show-error -H "X-Vault-Token: $TOKEN" -X GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb)
+	# check if curl work
+	CURL_HTTP_CODE=$(curl -o /dev/null --write-out "%{http_code}" -s --show-error -H "X-Vault-Token: $TOKEN" -X GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb)
 
-# check if YQ work
-YQ_OUT=$(curl -s --show-error -H "X-Vault-Token: $TOKEN" -X GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb | yq '.data.password')
-YQ_EXIT_CODE=$?
+	# check if YQ work
+	YQ_OUT=$(curl -s --show-error -H "X-Vault-Token: $TOKEN" -X GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb | yq '.data.password')
+	YQ_EXIT_CODE=$?
 
-# check if CURL or YQ return no-zero code
+	# check if CURL or YQ return no-zero code
 	if [ $CURL_HTTP_CODE != 200 ] || [ $YQ_EXIT_CODE -ne 0 ] ; then
 		logger "edgex-ekuiper:redis-token-setup: could not use Vault token to query Redis credentials"
 		exit 1
