31c3aac16dd35426f1983f30732449ff5b4a5113
Mengyi Wang <mengyi.wang@canonical.com>
2022-03-03 17:25:51 +0100

    Improve error checks and logs (Part 2)
    - take the username from vault as well as password
    - update function name
    - combine exit code handling 
    - improve logs and comments
    
    Signed-off-by: Mengyi Wang <mengyi.wang@canonical.com>

--- snap/local/runtime-helpers/bin/redis-token-setup.sh
+++ snap/local/runtime-helpers/bin/redis-token-setup.sh
@@ -7,11 +7,13 @@ REDIS_TOKEN_FILE=$SNAP_DATA/edgex-ekuiper/redis.yaml
 SOURCE_FILE=$SNAP_DATA/etc/sources/edgex.yaml 
 CONNECTIONS_FILE=$SNAP_DATA/etc/connections/connection.yaml
 
-YQ_EXIT_CODE()
+handle_error()
 {
 	local EXIT_CODE=$1
+	local ITEM=$2
+	local RESPONSE=$3
 	if [ $EXIT_CODE -ne 0 ] ; then
-		logger --stderr "edgex-ekuiper:redis-token-setup: yq return no-zero exit code: $EXIT_CODE"
+		logger --stderr "edgex-ekuiper:redis-token-setup: $ITEM return no-zero exit code: $EXIT_CODE, with response: $RESPONSE"
 		exit 1
 	fi
 }
@@ -21,22 +23,15 @@ if [ -f "$VAULT_TOKEN_FILE" ] ; then
 	# get Vault token and create redis.yaml
 	logger "edgex-ekuiper:redis-token-setup: using Vault token to query Redis token"
 	TOKEN=$(yq "$VAULT_TOKEN_FILE" | yq ' .auth.client_token')
-	EXIT_CODE=$?
-	YQ_EXIT_CODE $EXIT_CODE 
+	handle_error $? "yq" $TOKEN
 
-    # check if CURL works
+    # check CURL's exit code
 	CURL_RES=$(curl --silent --write-out "%{http_code}" \
 	--header "X-Vault-Token: $TOKEN" \
 	--request GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb)
+    handle_error $? "curl" $CURL_RES
 
-	# check CURL's exit code
-	EXIT_CODE=$?
-	if [ $EXIT_CODE -ne 0 ] ; then
-		logger --stderr "edgex-ekuiper:redis-token-setup: curl return no-zero exit code: $EXIT_CODE, with response: $CURL_RES"
-		exit 1
-	fi
-
-	# check CURL's http code
+	# check response http code
 	HTTP_CODE="${CURL_RES:${#CURL_RES}-3}"
 	if [ $HTTP_CODE -ne 200 ] ; then
 	  logger --stderr "edgex-ekuiper:redis-token-setup: curl return error with http code: $HTTP_CODE, with response: $CURL_RES"
@@ -51,31 +46,21 @@ if [ -f "$VAULT_TOKEN_FILE" ] ; then
 	fi
 
 	# process the reponse and check if yq works
-	YQ_RES=$($RES| yq '.data.password')
-	EXIT_CODE=$?
-	YQ_EXIT_CODE $EXIT_CODE
-
-	# put Redis token into redis.yaml
-	logger "edgex-ekuiper:redis-token-setup: sending Redis token to $REDIS_TOKEN_FILE"
-	echo $YQ_OUT > "$REDIS_TOKEN_FILE"
-	logger "edgex-ekuiper:redis-token-setup: sent Redis token to $REDIS_TOKEN_FILE"
-	REDIS_PASSWORD=$(yq "$REDIS_TOKEN_FILE")
-	EXIT_CODE=$?
-	YQ_EXIT_CODE $EXIT_CODE
+	REDIS_CREDENTIALS=$(echo $BODY| yq '.data')
+	handle_error $? "yq" $REDIS_CREDENTIALS
 
 	# pass generated Redis credentials to configuration files
-	logger "edgex-ekuiper:redis-token-setup: sending Redis token to $SOURCE_FILE"
-	yq -i '.default += {"optional":{"Username":"Redis"}+{"Password":"'$REDIS_PASSWORD'"}}' "$SOURCE_FILE"
-	EXIT_CODE=$?
-	YQ_EXIT_CODE $EXIT_CODE
-	logger "edgex-ekuiper:redis-token-setup: sending Redis token to $CONNECTIONS_FILE"
-	yq -i '.edgex.redisMsgBus += {"optional":{"Username":"Redis"}+{"Password":"'$REDIS_PASSWORD'"}}' "$CONNECTIONS_FILE"
-	EXIT_CODE=$?
-	YQ_EXIT_CODE $EXIT_CODE
+	logger "edgex-ekuiper:redis-token-setup: adding Redis credentials to $SOURCE_FILE"
+	YQ_RES=$(yq -i '.default += {"optional":'"$REDIS_CREDENTIALS"'}' "$SOURCE_FILE")
+	handle_error $? "yq" $YQ_RES
+	
+	logger "edgex-ekuiper:redis-token-setup: adding Redis credentials to $CONNECTIONS_FILE"
+	YQ_RES=$(yq -i '.edgex.redisMsgBus += '"$REDIS_CREDENTIALS"'' "$CONNECTIONS_FILE")
+	handle_error $? "yq" $YQ_RES
 
-	logger "edgex-ekuiper:redis-token-setup: start edgex-ekuiper with edgexfoundry Vault token"
+	logger "edgex-ekuiper:redis-token-setup: configured eKuiper to authenticate with Redis, using credentials fetched from Vault"
 else
-	logger "edgex-ekuiper:connect-slot-edgex-secretstore-token: start edgex-ekuiper without edgexfoundry Vault token as it is not available"
+	logger --stderr "edgex-ekuiper:redis-token-setup: unable to configure eKuiper to authenticate with Redis: unable to query Redis token from Vault: Vault token not available"
 fi
 
 exec "$@"
