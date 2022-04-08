60f7ba8500ddabd6eddef2a477321d89d4480b59
Mengyi <mengyi.wang@canonical.com>
2022-04-08 14:09:52 +0200

    Add snap option to allow disabling redis token setup (#20)
    * Add startup config option for `edgex-ekuiper`
    
    * Decrease `restart-delay`
    
    * Add config option validation
    
    Co-authored-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- snap/local/runtime-helpers/bin/setup-redis-credentials.sh
+++ snap/local/runtime-helpers/bin/setup-redis-credentials.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 LOG_PREFIX=$SNAP_INSTANCE_NAME:configure-kuiper
-
+EDGEX_SECURITY=$(snapctl get edgex-security)
 logger "$LOG_PREFIX: started"
 
 for var in VAULT_TOKEN_FILE SOURCE_FILE CONNECTION_FILE ; do
@@ -24,52 +22,58 @@ handle_error()
 	fi
 }
 
-# use Vault token query Redis token, access edgexfoundry secure Message Bus
-if [ -f "$VAULT_TOKEN_FILE" ] ; then
-	# get Vault token and create redis.yaml
-	logger "$LOG_PREFIX: using Vault token to query Redis token"
-	TOKEN=$(yq "$VAULT_TOKEN_FILE" | yq ' .auth.client_token')
-	handle_error $? "yq" $TOKEN
-
-	# check CURL's exit code
-	CURL_RES=$(curl --silent --write-out "%{http_code}" \
-	--header "X-Vault-Token: $TOKEN" \
-	--request GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb)
-	handle_error $? "curl" $CURL_RES
-
-	# check response http code
-	HTTP_CODE="${CURL_RES:${#CURL_RES}-3}"
-	if [ $HTTP_CODE -ne 200 ] ; then
-		logger --stderr "$LOG_PREFIX: http error $HTTP_CODE, with response: $CURL_RES"
-		exit 1
-	fi
-
-	# get CURL's reponse
-	if [ ${#CURL_RES} -eq 3 ]; then
-		logger --stderr "$LOG_PREFIX: unexpected http response with empty body"
-		exit 1
-	else
-		BODY="${CURL_RES:0:${#CURL_RES}-3}"
-	fi
-
-	# process the reponse and check if yq works
-	REDIS_USER=$(echo $BODY| yq '.data.username')
-	handle_error $? "yq" $REDIS_USER
-	REDIS_PASS=$(echo $BODY| yq '.data.password')
-	handle_error $? "yq" $REDIS_PASS
-
-	# pass generated Redis credentials to configuration files
-	logger "$LOG_PREFIX: adding Redis credentials to $SOURCE_FILE"
-	YQ_RES=$(yq -i '.default += {"optional":{"Username":"'$REDIS_USER'"}+{"Password":"'$REDIS_PASS'"}}' "$SOURCE_FILE")
-	handle_error $? "yq" $YQ_RES
-	
-	logger "$LOG_PREFIX: adding Redis credentials to $CONNECTION_FILE"
-	YQ_RES=$(yq -i '.edgex.redisMsgBus += {"optional":{"Username":"'$REDIS_USER'"}+{"Password":"'$REDIS_PASS'"}}' "$CONNECTION_FILE")
-	handle_error $? "yq" $YQ_RES
-
-	logger "$LOG_PREFIX: configured eKuiper to authenticate with Redis, using credentials fetched from Vault"
+if [ "$EDGEX_SECURITY" == "off" ] ; then
+	logger "$LOG_PREFIX: EDGEX_SECURITY set to off, ekuiper start without edgexfoundry authentication"
 else
-	logger --stderr "$LOG_PREFIX: unable to configure eKuiper to authenticate with Redis: unable to query Redis token from Vault: Vault token not available"
+	logger "$LOG_PREFIX: EDGEX_SECURITY set to on by default, ekuiper start to get edgexfoundry authentication"
+	# use Vault token query Redis token, access edgexfoundry secure Message Bus
+	if [ -f "$VAULT_TOKEN_FILE" ] ; then
+		# get Vault token and create redis.yaml
+		logger "$LOG_PREFIX: using Vault token to query Redis token"
+		TOKEN=$(yq "$VAULT_TOKEN_FILE" | yq ' .auth.client_token')
+		handle_error $? "yq" $TOKEN
+
+		# check CURL's exit code
+		CURL_RES=$(curl --silent --write-out "%{http_code}" \
+		--header "X-Vault-Token: $TOKEN" \
+		--request GET http://localhost:8200/v1/secret/edgex/edgex-ekuiper/redisdb)
+		handle_error $? "curl" $CURL_RES
+
+		# check response http code
+		HTTP_CODE="${CURL_RES:${#CURL_RES}-3}"
+		if [ $HTTP_CODE -ne 200 ] ; then
+			logger --stderr "$LOG_PREFIX: http error $HTTP_CODE, with response: $CURL_RES"
+			exit 1
+		fi
+
+		# get CURL's reponse
+		if [ ${#CURL_RES} -eq 3 ]; then
+			logger --stderr "$LOG_PREFIX: unexpected http response with empty body"
+			exit 1
+		else
+			BODY="${CURL_RES:0:${#CURL_RES}-3}"
+		fi
+
+		# process the reponse and check if yq works
+		REDIS_USER=$(echo $BODY| yq '.data.username')
+		handle_error $? "yq" $REDIS_USER
+		REDIS_PASS=$(echo $BODY| yq '.data.password')
+		handle_error $? "yq" $REDIS_PASS
+
+		# pass generated Redis credentials to configuration files
+		logger "$LOG_PREFIX: adding Redis credentials to $SOURCE_FILE"
+		YQ_RES=$(yq -i '.default += {"optional":{"Username":"'$REDIS_USER'"}+{"Password":"'$REDIS_PASS'"}}' "$SOURCE_FILE")
+		handle_error $? "yq" $YQ_RES
+		
+		logger "$LOG_PREFIX: adding Redis credentials to $CONNECTION_FILE"
+		YQ_RES=$(yq -i '.edgex.redisMsgBus += {"optional":{"Username":"'$REDIS_USER'"}+{"Password":"'$REDIS_PASS'"}}' "$CONNECTION_FILE")
+		handle_error $? "yq" $YQ_RES
+
+		logger "$LOG_PREFIX: configured eKuiper to authenticate with Redis, using credentials fetched from Vault"
+	else
+		logger --stderr "$LOG_PREFIX: unable to configure eKuiper to authenticate with Redis: unable to query Redis token from Vault: Vault token not available. Exiting..."
+		exit 1
+	fi
 fi
 
 exec "$@"
