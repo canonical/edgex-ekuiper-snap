d871f33511a1561d2980ff361b3a136af718d2b2
Mengyi Wang <mengyi.wang@canonical.com>
2022-10-20 11:24:43 +0200

    Update service name in env file

--- snap/local/bin/source-env-file.sh
+++ snap/local/bin/source-env-file.sh
@@ -7,18 +7,18 @@ ARGV=($@)
 BINPATH="${ARGV[0]}"
 
 # binary name == service name/key
-SERVICE="kuiper"
+SERVICE=$(basename "$BINPATH")
 SERVICE_ENV="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
 TAG="edgex-$SERVICE."$(basename "$0")
 
 # replace one underscore with two underscores
-sed -i 's/_/__/g' "$SERVICE_ENV"
+sed -i 's/_/__/g' "$SNAP_DATA/config/kuiper/res/kuiper.env"
 
 if [ -f "$SERVICE_ENV" ]; then
     logger --tag=$TAG "sourcing $SERVICE_ENV"
     set -o allexport
     source "$SERVICE_ENV" set
-    set +o allexport 
+    set +o allexport
 fi
 
 exec "$@"
