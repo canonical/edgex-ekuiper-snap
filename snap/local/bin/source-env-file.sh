268feea2a89e707c45be6e456657d607ad73e19d
Mengyi Wang <mengyi.wang@canonical.com>
2023-02-07 16:29:39 +0100

    Update service name in source-env-file (#48)

--- snap/local/bin/source-env-file.sh
+++ snap/local/bin/source-env-file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-SERVICE="kuiper"
+SERVICE="kuiperd"
 SERVICE_ENV="$SNAP_DATA/config/$SERVICE/res/$SERVICE.env"
-TAG="edgex-$SERVICE."$(basename "$0")
+TAG="$SNAP_INSTANCE_NAME."$(basename "$0")
 
 if [ -f "$SERVICE_ENV" ]; then
     logger --tag=$TAG "Sourcing $SERVICE_ENV"
