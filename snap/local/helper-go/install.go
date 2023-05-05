c6b9687ca1a0d2ee2dd90d903e3079f73f53265e
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-05-05 14:44:43 +0200

    Copy without creating nested directory

--- snap/local/helper-go/install.go
+++ snap/local/helper-go/install.go
@@ -27,14 +27,13 @@ import (
 
 // installConfig copies all config files from $SNAP to $SNAP_DATA.
 func installConfig() error {
-	path := "/"
 
-	out, err := exec.Command("cp", "--recursive -v", env.Snap+path, env.SnapData+path).CombinedOutput()
+	out, err := exec.Command(
+		"cp", "--recursive", "--no-target-directory", "--verbose", env.Snap, env.SnapData).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("%s: %s", out, err)
 	}
-
-	log.Infof("Out: %s", out)
+	log.Infof("Copied %s", out)
 
 	return nil
 }
